53bd42792356e3939fcb58868d6a99918c47d56e
Michael Pabody <michael.pabody@gmail.com>
2019-09-02 13:21:12 -0400

    NoteService class with CreateNote/GetNotes methods, change index/create GET methods in NoteController

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,4 +1,6 @@
 ﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,10 @@ namespace ElevenNote.WebMVC.Controllers
         // GET: Note
         public ActionResult Index()
         {
-            var model = new ElevenNote.Models.NoteListItem[0];
+            var userID = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userID);
+            var model = service.GetNotes();
+
             return View(model);
         }
 
@@ -29,9 +34,15 @@ namespace ElevenNote.WebMVC.Controllers
         {
             if (ModelState.IsValid)
             {
-
+                return View(model);
             }
-            return View(model);
+
+            var userID = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userID);
+
+            service.CreateNote(model);
+
+            return RedirectToAction("Index");
         }
     }
 }
