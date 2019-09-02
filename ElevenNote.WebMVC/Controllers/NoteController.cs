42e1e5acfc05637e90add680de31ffe2e6849b4e
Michael Pabody <michael.pabody@gmail.com>
2019-09-02 12:27:34 -0400

    NoteClass added, NoteController GET method, Create view, db migration, Create Note on site, Note tab on navbar

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevenNote.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,23 @@ namespace ElevenNote.WebMVC.Controllers
         public ActionResult Index()
         {
             var model = new ElevenNote.Models.NoteListItem[0];
+            return View(model);
+        }
+
+        // GET : Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
             return View(model);
         }
     }
