c0a03f331cf347097b40b2b5a037f11ae5964593
Michael Pabody <michael.pabody@gmail.com>
2019-09-03 18:57:18 -0400

    NoteEdit class, UpdateNote/Edit methods

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -55,6 +55,20 @@ namespace ElevenNote.WebMVC.Controllers
             return View(model);
         }
 
+        public ActionResult Edit(int id)
+        {
+            var service = CreateNoteService();
+            var detail = service.GetNoteByID(id);
+            var model =
+                new NoteEdit
+                {
+                    NoteID = detail.NoteID,
+                    Title = detail.Title,
+                    Content = detail.Content
+                };
+            return View(model);
+        }
+
         private NoteService CreateNoteService()
         {
             var userID = Guid.Parse(User.Identity.GetUserId());
